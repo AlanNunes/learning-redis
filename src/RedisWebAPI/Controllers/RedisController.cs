907ff6d69ce6f48bd737bdc3284615f352fb5529
AlanNunes <alann.625@gmail.com>
2021-09-26 03:31:28 -0300

    first commit

---
+++ src/RedisWebAPI/Controllers/RedisController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+
+namespace RedisWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RedisController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index(
+            [FromServices]IDistributedCache cache)
+        {
+            string nome = cache.GetString("nome");
+            if (string.IsNullOrEmpty(nome))
+            {
+                DistributedCacheEntryOptions opcoesCache = new();
+                opcoesCache.SetAbsoluteExpiration(TimeSpan.FromMinutes(1));
+                cache.SetString("nome", "Alan Nunes da Silva", opcoesCache);
+            }
+
+            return Ok(nome);
+        }
+    }
+}
